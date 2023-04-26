3c810fdcf25e55b1022f381f1501c8da1c315f52
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-04-26 11:48:42 +0100

    Added Update Integration Tests

--- tests/FamilyHubs.ReferralApi.Integration.Tests/WhenUsingUpdateReferral.cs
+++ tests/FamilyHubs.ReferralApi.Integration.Tests/WhenUsingUpdateReferral.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using FamilyHubs.ReferralApi.Core.Commands.UpdateReferral;
+﻿using FamilyHubs.ReferralApi.Core.Commands.UpdateReferral;
 using FamilyHubs.ReferralApi.Data.Entities;
 using FamilyHubs.ReferralCommon.Shared.Dto;
 using FluentAssertions;
@@ -31,8 +30,90 @@ public class WhenUsingUpdateReferral : DataIntegrationTestBase
         var actualService = TestDbContext.Referrals.SingleOrDefault(s => s.Id == referral.Id);
         actualService.Should().NotBeNull();
         actualService!.ReferenceNumber.Should().Be(referral.ReferenceNumber);
+        actualService!.ReasonForSupport.Should().Be(referral.ReasonForSupport);
+        actualService!.EngageWithFamily.Should().Be(referral.EngageWithFamily);
 
     }
+
+    [Fact]
+    public async Task ThenUpdateRecipientOnly()
+    {
+        await CreateReferral();
+        var referral = TestDataProvider.GetReferralDto();
+        referral.RecipientDto.AddressLine1 = "Unit Test Address Line 1";
+        referral.RecipientDto.AddressLine2 = "Unit Test Address Line 2";
+        referral.RecipientDto.TownOrCity = "Unit Test Town of City";
+        referral.RecipientDto.PostCode = "12345";
+        referral.RecipientDto.TextPhone = "1234567890";
+        referral.RecipientDto.Telephone = "1234567890";
+        referral.RecipientDto.Email = "someone@email.com";
+        var expected = referral.RecipientDto;
+
+        UpdateReferralCommand command = new(referral.Id, referral);
+        UpdateReferralCommandHandler handler = new(TestDbContext, Mapper, new Mock<ILogger<UpdateReferralCommandHandler>>().Object);
+
+        //Act
+        var result = await handler.Handle(command, new System.Threading.CancellationToken());
+
+        //Assert
+        result.Should().NotBe(0);
+        result.Should().Be(referral.Id);
+        var actualService = TestDbContext.Referrals.SingleOrDefault(s => s.Id == referral.Id);
+        actualService.Should().NotBeNull();
+        actualService!.Recipient.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public async Task ThenUpdateReferrerOnly()
+    {
+        await CreateReferral();
+        var referral = TestDataProvider.GetReferralDto();
+        referral.ReferrerDto.EmailAddress = "unittestsomeone@email.com";
+        var expected = referral.ReferrerDto;
+
+        UpdateReferralCommand command = new(referral.Id, referral);
+        UpdateReferralCommandHandler handler = new(TestDbContext, Mapper, new Mock<ILogger<UpdateReferralCommandHandler>>().Object);
+
+        //Act
+        var result = await handler.Handle(command, new System.Threading.CancellationToken());
+
+        //Assert
+        result.Should().NotBe(0);
+        result.Should().Be(referral.Id);
+        var actualService = TestDbContext.Referrals.SingleOrDefault(s => s.Id == referral.Id);
+        actualService.Should().NotBeNull();
+        actualService!.Referrer.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public async Task ThenUpdateServiceAndOrganisationOnly()
+    {
+        await CreateReferral();
+        var referral = TestDataProvider.GetReferralDto();
+        referral.ReferralServiceDto.Name = "Unit Test Name";
+        referral.ReferralServiceDto.Description = "Unit Test Description";
+        referral.ReferralServiceDto.ReferralOrganisationDto.Name = "Unit Test Org Name";
+        referral.ReferralServiceDto.ReferralOrganisationDto.Description = "Unit Test Org Description";
+        var expected = referral.ReferralServiceDto;
+
+        UpdateReferralCommand command = new(referral.Id, referral);
+        UpdateReferralCommandHandler handler = new(TestDbContext, Mapper, new Mock<ILogger<UpdateReferralCommandHandler>>().Object);
+
+        //Act
+        var result = await handler.Handle(command, new System.Threading.CancellationToken());
+
+        //Assert
+        result.Should().NotBe(0);
+        result.Should().Be(referral.Id);
+        var actualService = TestDbContext.Referrals.SingleOrDefault(s => s.Id == referral.Id);
+        actualService.Should().NotBeNull();
+        actualService!.ReferralService.Name.Should().Be(expected.Name);
+        actualService!.ReferralService.Description.Should().Be(expected.Description);
+        actualService!.ReferralService.ReferralOrganisation.Name.Should().Be(expected.ReferralOrganisationDto.Name);
+        actualService!.ReferralService.ReferralOrganisation.Description.Should().Be(expected.ReferralOrganisationDto.Description);
+    }
+
+
     public async Task<ReferralDto> CreateReferral(ReferralDto? newReferral = null)
     {
         
