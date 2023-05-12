4b9d986bb937d9c6e6053657c861019cfa24c4d3
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-05-12 16:08:57 +0100

    Added Status

---
+++ src/FamilyHubs.Referral.Data/Entities/Status.cs
@@ -0,0 +1,9 @@
+﻿namespace FamilyHubs.Referral.Data.Entities;
+
+//New, Opened, Accepted, Declined in this sort order 
+
+public class Status : EntityBase<long>
+{
+    public string Name { get; set; }
+    public byte SortOrder { get; set; }
+}
