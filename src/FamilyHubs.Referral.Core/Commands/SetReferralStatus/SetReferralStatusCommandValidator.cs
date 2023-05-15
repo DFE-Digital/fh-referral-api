9dff64279f89cd84db52ff25ff0f993ac5a3350c
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-05-15 12:37:44 +0100

    Added SetStatusCommandValidator

---
+++ src/FamilyHubs.Referral.Core/Commands/SetReferralStatus/SetReferralStatusCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace FamilyHubs.Referral.Core.Commands.SetReferralStatus;
+
+public class SetReferralStatusCommandValidator : AbstractValidator<SetReferralStatusCommand>
+{
+    public SetReferralStatusCommandValidator()
+    {
+
+        RuleFor(v => v.ReferralId)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(v => v.Status)
+            .MinimumLength(1)
+            .MaximumLength(50)
+            .NotNull()
+            .NotEmpty();
+    }
+}
