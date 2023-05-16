8013d3fb883f41a15d59943dd128378165526215
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-05-16 14:00:49 +0100

    Added required attribute to SortOrder

--- src/FamilyHubs.Referral.Data/Entities/ReferralStatus.cs
+++ src/FamilyHubs.Referral.Data/Entities/ReferralStatus.cs
@@ -3,6 +3,6 @@
 public class ReferralStatus : EntityBase<long>
 {
     public required string Name { get; set; } = default!;
-    public byte SortOrder { get; set; }
+    public required byte SortOrder { get; set; }
 
 }
