eaba3d9602ee0c022d8d33f68b581aae805fb7af
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-06-15 19:04:16 +0100

    Fixed Update

--- src/FamilyHubs.Referral.Data/Entities/Referral.cs
+++ src/FamilyHubs.Referral.Data/Entities/Referral.cs
@@ -1,4 +1,5 @@
 ﻿using EncryptColumn.Core.Attribute;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FamilyHubs.Referral.Data.Entities;
 
@@ -7,6 +8,10 @@ public class Referral : EntityBase<long>
     public required string ReasonForSupport { get; set; }
     public required string EngageWithFamily { get; set; }
     public string? ReasonForDecliningSupport { get; set; }
+
+    
+    public long StatusId { get; set; }
+    [ForeignKey("StatusId")]
     public virtual required ReferralStatus Status { get; set; }
     public virtual required Recipient Recipient { get; set; }
 
