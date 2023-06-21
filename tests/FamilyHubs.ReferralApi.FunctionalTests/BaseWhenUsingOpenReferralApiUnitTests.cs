02df92d0460104f10d11590e7921be8c2f246a3f
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-06-21 13:10:01 +0100

    Modified BaseWhenUsingOpenReferralApiUnitTests

--- tests/FamilyHubs.ReferralApi.FunctionalTests/BaseWhenUsingOpenReferralApiUnitTests.cs
+++ tests/FamilyHubs.ReferralApi.FunctionalTests/BaseWhenUsingOpenReferralApiUnitTests.cs
@@ -12,76 +12,86 @@ namespace FamilyHubs.Referral.FunctionalTests;
 #pragma warning disable S3881
 public abstract class BaseWhenUsingOpenReferralApiUnitTests : IDisposable
 {
-    protected readonly HttpClient Client;
+    protected HttpClient Client;
     private readonly CustomWebApplicationFactory _webAppFactory;
     private bool _disposed;
-    protected readonly JwtSecurityToken _token;
-    protected readonly JwtSecurityToken _vcstoken;
-    protected readonly JwtSecurityToken _forbiddentoken;
+    protected readonly JwtSecurityToken? _token;
+    protected readonly JwtSecurityToken? _vcstoken;
+    protected readonly JwtSecurityToken? _forbiddentoken;
     private readonly IConfiguration _configuration;
 
     protected BaseWhenUsingOpenReferralApiUnitTests()
     {
         _disposed = false;
 
+       
         var configuration = new ConfigurationBuilder()
-            .AddUserSecrets<Program>()
-            .Build();
+        .AddUserSecrets<Program>()
+        .Build();
 
         _configuration = configuration;
 
-        if (configuration == null)
+        bool canCreateTokens = false;
+        if (configuration != null && configuration.GetValue<string>("GovUkOidcConfiguration:BearerTokenSigningKey") != null)
         {
-            return;
+            canCreateTokens = true;
+            var jti = Guid.NewGuid().ToString();
+            var key = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(configuration["GovUkOidcConfiguration:BearerTokenSigningKey"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+            _token = new JwtSecurityToken(
+                claims: new List<Claim>
+                    {
+                new Claim("sub", configuration["GovUkOidcConfiguration:Oidc:ClientId"] ?? ""),
+                new Claim("jti", jti),
+                new Claim(ClaimTypes.Role, RoleTypes.LaProfessional),
+                new Claim(FamilyHubsClaimTypes.OrganisationId, "3")
+
+                    },
+                signingCredentials: creds,
+            expires: DateTime.UtcNow.AddMinutes(5)
+                );
+
+            _vcstoken = new JwtSecurityToken(
+                claims: new List<Claim>
+                    {
+                new Claim("sub", configuration["GovUkOidcConfiguration:Oidc:ClientId"] ?? ""),
+                new Claim("jti", jti),
+                new Claim(ClaimTypes.Role, RoleTypes.VcsProfessional),
+                new Claim(FamilyHubsClaimTypes.OrganisationId, "1")
+
+                    },
+                signingCredentials: creds,
+            expires: DateTime.UtcNow.AddMinutes(5)
+                );
+
+            _forbiddentoken = new JwtSecurityToken(
+                claims: new List<Claim>
+                    {
+                new Claim("sub", configuration["GovUkOidcConfiguration:Oidc:ClientId"] ?? ""),
+                new Claim("jti", jti),
+                new Claim(ClaimTypes.Role, RoleTypes.VcsProfessional),
+                new Claim(FamilyHubsClaimTypes.OrganisationId, "-1")
+
+                    },
+                signingCredentials: creds,
+            expires: DateTime.UtcNow.AddMinutes(5)
+                );
+
         }
 
-        var jti = Guid.NewGuid().ToString();
-        var key = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(configuration["GovUkOidcConfiguration:BearerTokenSigningKey"]!));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
-        _token = new JwtSecurityToken(
-            claims: new List<Claim>
-               {
-                    new Claim("sub", configuration["GovUkOidcConfiguration:Oidc:ClientId"] ?? ""),
-                    new Claim("jti", jti),
-                    new Claim(ClaimTypes.Role, RoleTypes.LaProfessional),
-                    new Claim(FamilyHubsClaimTypes.OrganisationId, "3")
-
-               },
-            signingCredentials: creds,
-        expires: DateTime.UtcNow.AddMinutes(5)
-            );
-
-        _vcstoken = new JwtSecurityToken(
-            claims: new List<Claim>
-               {
-                    new Claim("sub", configuration["GovUkOidcConfiguration:Oidc:ClientId"] ?? ""),
-                    new Claim("jti", jti),
-                    new Claim(ClaimTypes.Role, RoleTypes.VcsProfessional),
-                    new Claim(FamilyHubsClaimTypes.OrganisationId, "1")
-
-               },
-            signingCredentials: creds,
-        expires: DateTime.UtcNow.AddMinutes(5)
-            );
-
-        _forbiddentoken = new JwtSecurityToken(
-            claims: new List<Claim>
-               {
-                    new Claim("sub", configuration["GovUkOidcConfiguration:Oidc:ClientId"] ?? ""),
-                    new Claim("jti", jti),
-                    new Claim(ClaimTypes.Role, RoleTypes.VcsProfessional),
-                    new Claim(FamilyHubsClaimTypes.OrganisationId, "-1")
-
-               },
-            signingCredentials: creds,
-        expires: DateTime.UtcNow.AddMinutes(5)
-            );
+       
 
         _webAppFactory = new CustomWebApplicationFactory();
         _webAppFactory.SetupTestDatabaseAndSeedData();
 
         Client = _webAppFactory.CreateDefaultClient();
         Client.BaseAddress = new Uri("https://localhost:7192/");
+
+        if (!canCreateTokens)
+            _configuration = default!;
+
+
+
     }
 
     protected virtual void Dispose(bool disposing)
