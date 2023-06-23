a953cd99fa2ab16ad309757e2a33e30d52e64c68
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-06-23 11:46:45 +0100

    Added Migration for adding service url

---
+++ src/FamilyHubs.Referral.Data/Migrations/20230623104212_AddServiceUrlToSchema.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyHubs.Referral.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddServiceUrlToSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "ReferralServices",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "ReferralServices");
+        }
+    }
+}
