5750d9479eabe5cb92af33a8ebd5569b7f7a27f2
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-06-28 09:58:29 +0100

    Added Migration for ReferrerTelephone

---
+++ src/FamilyHubs.Referral.Data/Migrations/20230628085620_AddReferralPhoneNoToSchema.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyHubs.Referral.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReferralPhoneNoToSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReferrerTelephone",
+                table: "Referrals",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferrerTelephone",
+                table: "Referrals");
+        }
+    }
+}
