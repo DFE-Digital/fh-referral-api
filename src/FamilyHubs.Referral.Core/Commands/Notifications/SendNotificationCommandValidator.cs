576375c02fd18bdcec7ea406630b43fe348071be
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-07-10 08:56:57 +0100

    Added ThenTheOpenReferralIsCreatedAndNotificationIsSent

---
+++ src/FamilyHubs.Referral.Core/Commands/Notifications/SendNotificationCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FamilyHubs.Referral.Core.Commands.Notifications;
+using FluentValidation;
+
+namespace FamilyHubs.Referral.Core.Commands.CreateReferral;
+
+public class SendNotificationCommandValidator : AbstractValidator<SendNotificationCommand>
+{
+    public SendNotificationCommandValidator()
+    {
+        RuleFor(v => v.ReferralId)
+            .GreaterThan(0);
+    }
+}
--- tests/FamilyHubs.ReferralApi.FunctionalTests/WhenUsingReferralsApiUnitTests.cs
+++ tests/FamilyHubs.ReferralApi.FunctionalTests/WhenUsingReferralsApiUnitTests.cs
@@ -17,6 +17,119 @@ public class WhenUsingReferralsApiUnitTests : BaseWhenUsingOpenReferralApiUnitTe
     const string JsonService = "{\"Id\":\"ba1cca90-b02a-4a0b-afa0-d8aed1083c0d\",\"Name\":\"Test County Council\",\"Description\":\"Test County Council\",\"Logo\":null,\"Uri\":\"https://www.test.gov.uk/\",\"Url\":\"https://www.test.gov.uk/\",\"Services\":[{\"Id\":\"c1b5dd80-7506-4424-9711-fe175fa13eb8\",\"Name\":\"Test Organisation for Children with Tracheostomies\",\"Description\":\"Test Organisation for for Children with Tracheostomies is a national self help group operating as a registered charity and is run by parents of children with a tracheostomy and by people who sympathise with the needs of such families. ACT as an organisation is non profit making, it links groups and individual members throughout Great Britain and Northern Ireland.\",\"Accreditations\":null,\"Assured_date\":null,\"Attending_access\":null,\"Attending_type\":null,\"Deliverable_type\":null,\"Status\":\"active\",\"Url\":\"www.testservice.com\",\"Email\":\"support@testservice.com\",\"Fees\":null,\"ServiceDelivery\":[{\"Id\":\"14db2aef-9292-4afc-be09-5f6f43765938\",\"ServiceDelivery\":2}],\"Eligibilities\":[{\"Id\":\"Test9109Children\",\"Eligibility\":\"\",\"Maximum_age\":0,\"Minimum_age\":13}],\"Contacts\":[{\"Id\":\"5eac5cb6-cc7e-444d-a29b-76ccb85be866\",\"Title\":\"Service\",\"Name\":\"\",\"Phones\":[{\"Id\":\"1568\",\"Number\":\"01827 65779\"}]}],\"Cost_options\":[],\"Languages\":[{\"Id\":\"442a06cd-aa14-4ea3-9f11-b45c1bc4861f\",\"Language\":\"English\"}],\"Service_areas\":[{\"Id\":\"68af19cd-bc81-4585-99a2-85a2b0d62a1d\",\"Service_area\":\"National\",\"Extent\":null,\"Uri\":\"http://statistics.data.gov.uk/id/statistical-geography/K02000001\"}],\"Service_at_locations\":[{\"Id\":\"Test1749\",\"Location\":{\"Id\":\"a878aadc-6097-4a0f-b3e1-77fd4511175d\",\"Name\":\"\",\"Description\":\"\",\"Latitude\":52.6312,\"Longitude\":-1.66526,\"Physical_addresses\":[{\"Id\":\"1076aaa8-f99d-4395-8e4f-c0dde8095085\",\"Address_1\":\"75 Sheepcote Lane\",\"City\":\", Stathe, Tamworth, Staffordshire, \",\"Postal_code\":\"B77 3JN\",\"Country\":\"England\",\"State_province\":null}]}}],\"Service_taxonomys\":[{\"Id\":\"Test9107\",\"Taxonomy\":{\"Id\":\"Test bccsource:Organisation\",\"Name\":\"Organisation\",\"Vocabulary\":\"Test BCC Data Sources\",\"Parent\":null}},{\"Id\":\"Test9108\",\"Taxonomy\":{\"Id\":\"Test bccprimaryservicetype:38\",\"Name\":\"Support\",\"Vocabulary\":\"Test BCC Primary Services\",\"Parent\":null}},{\"Id\":\"Test9109\",\"Taxonomy\":{\"Id\":\"Test bccagegroup:37\",\"Name\":\"Children\",\"Vocabulary\":\"Test BCC Age Groups\",\"Parent\":null}},{\"Id\":\"Test9110\",\"Taxonomy\":{\"Id\":\"Testbccusergroup:56\",\"Name\":\"Long Term Health Conditions\",\"Vocabulary\":\"Test BCC User Groups\",\"Parent\":null}}]}]}";
 #endif
 
+    //Only run when there is a need to test
+    //Use @education.gov.uk accounts but don't save to the repo
+    [Fact(Skip = "specific reason")]
+    public async Task ThenTheOpenReferralIsCreatedAndNotificationIsSent()
+    {
+        if (!IsRunningLocally() || Client == null)
+        {
+            // Skip the test if not running locally
+            Assert.True(true, "Test skipped because it is not running locally.");
+            return;
+        }
+
+        var command = new ReferralDto
+        {
+            Id = 3,
+            ReasonForSupport = "Reason For Support",
+            EngageWithFamily = "Engage With Family",
+            RecipientDto = new RecipientDto
+            {
+                Id = 3,
+                Name = "Fred Blogs",
+                Email = "FredBlog@email.com",
+                Telephone = "078123455",
+                TextPhone = "078123455",
+                AddressLine1 = "Unit Test Address Line 1",
+                AddressLine2 = "Unit Test Address Line 2",
+                TownOrCity = "Town or City",
+                County = "County",
+                PostCode = "B31 2TV"
+            },
+            ReferralUserAccountDto = new UserAccountDto
+            {
+                Id = 2,
+                EmailAddress = "<referrer with education account>",
+                Name = "<referrer with education account>",
+                PhoneNumber = "011 222 5555",
+                Team = "Social Work team North",
+                UserAccountRoles = new List<UserAccountRoleDto>(),
+                ServiceUserAccounts = new List<UserAccountServiceDto>(),
+                OrganisationUserAccounts = null,
+            },
+            Status = new ReferralStatusDto
+            {
+                Id = 1,
+                Name = "New",
+                SortOrder = 0
+            },
+            ReferralServiceDto = new ReferralServiceDto
+            {
+                Id = 3,
+                Name = "New Service",
+                Description = "Service Description",
+                Url = "www.service.com",
+                OrganisationDto = new OrganisationDto
+                {
+                    Id = 3,
+                    Name = "New Organisation",
+                    Description = "Organisation Description",
+                }
+            }
+
+        };
+
+        var referralDataDto = new ReferralDataDto
+        {
+            ReferralDto = command,
+            UserAccounts = new List<UserAccountDto>()
+            {
+                new UserAccountDto
+                {
+                    Id = 2,
+                    EmailAddress = "<referrer with education account>",
+                    PhoneNumber = "0121 777 8888",
+                    Team = "TBD",
+                    UserAccountRoles = new List<UserAccountRoleDto>()
+                    {
+                        new UserAccountRoleDto
+                        { 
+                            Id = 1,
+                            Role = new RoleDto
+                            {
+                                Name = "LaProfessional"
+                            },
+                            UserAccount = new UserAccountDto
+                            {
+                                EmailAddress = "<referrer with education account>",
+                            }
+                        }
+                    },
+                }
+            },
+        };
+
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri(Client.BaseAddress + "api/referralwithnotification"),
+            Content = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(referralDataDto), Encoding.UTF8, "application/json"),
+        };
+
+        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue($"Bearer", $"{new JwtSecurityTokenHandler().WriteToken(_token)}");
+
+        using var response = await Client.SendAsync(request);
+
+        response.EnsureSuccessStatusCode();
+
+        var stringResult = await response.Content.ReadAsStringAsync();
+        long.TryParse(stringResult, out var result);
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        result.Should().BeGreaterThan(0);
+    }
+
     [Fact]
     public async Task ThenReferralsByReferrerAreRetrieved()
     {
