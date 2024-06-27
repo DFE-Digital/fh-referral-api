7e8f6bbc785cabc63ee07bb33663ac4a10d7958f
Phil Davies <github@phildavies.co.uk>
2024-06-27 10:49:49 +0100

    add ConnectionRequestsSentMetric wip

---
+++ src/FamilyHubs.Referral.Data/Entities/Metrics/ConnectionRequestsSentMetric.cs
@@ -0,0 +1,12 @@
+﻿namespace FamilyHubs.Referral.Data.Entities.Metrics;
+
+public class ConnectionRequestsSentMetric : EntityBase<long>
+{
+    public long OrganisationId { get; set; }
+    public long UserAccountId { get; set; }
+    public DateTime ConnectionRequestSentTimestamp { get; set; }
+    public Guid CorrelationId { get; set; }
+    public long? ConnectionRequestId { get; set; }
+    public string? ConnectionRequestReference { get; set; }
+    public DateTime? ConnectionRequestReferenceSetTimestamp { get; set; }
+}
