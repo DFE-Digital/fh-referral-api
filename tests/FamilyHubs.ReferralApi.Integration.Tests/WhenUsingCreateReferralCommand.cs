17e3b7b7fcd5a4f75ecda9ab6e520d4d9243ca63
Phil Davies <github@phildavies.co.uk>
2024-07-01 12:25:35 +0100

    wip

--- tests/FamilyHubs.ReferralApi.Integration.Tests/WhenUsingCreateReferralCommand.cs
+++ tests/FamilyHubs.ReferralApi.Integration.Tests/WhenUsingCreateReferralCommand.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using System.Diagnostics;
 using FamilyHubs.ReferralService.Shared.CreateUpdateDto;
+using Microsoft.ApplicationInsights.Extensibility.W3C;
 
 namespace FamilyHubs.Referral.Integration.Tests
 {
@@ -19,27 +20,31 @@ namespace FamilyHubs.Referral.Integration.Tests
         public async Task ThenCreateReferral()
         {
             const long userOrganisationId = 123L;
-            var newReferral = new CreateReferralDto(TestDataProvider.GetReferralDto(), new ConnectionRequestsSentMetricDto(userOrganisationId));
+            var newReferral = new CreateReferralDto(TestDataProvider.GetReferralDto(),
+                new ConnectionRequestsSentMetricDto(userOrganisationId));
             Mock<IServiceDirectoryService> mockServiceDirectory = new Mock<IServiceDirectoryService>();
-            mockServiceDirectory.Setup(x => x.GetOrganisationById(It.IsAny<long>())).ReturnsAsync(new ServiceDirectory.Shared.Dto.OrganisationDto
-            {
-                Id = 2,
-                Name = "Organisation",
-                Description = "Organisation Description",
-                OrganisationType = ServiceDirectory.Shared.Enums.OrganisationType.VCFS,
-                AdminAreaCode = "AdminAreaCode"
-            });
+            mockServiceDirectory.Setup(x => x.GetOrganisationById(It.IsAny<long>())).ReturnsAsync(
+                new ServiceDirectory.Shared.Dto.OrganisationDto
+                {
+                    Id = 2,
+                    Name = "Organisation",
+                    Description = "Organisation Description",
+                    OrganisationType = ServiceDirectory.Shared.Enums.OrganisationType.VCFS,
+                    AdminAreaCode = "AdminAreaCode"
+                });
 
-            mockServiceDirectory.Setup(x => x.GetServiceById(It.IsAny<long>())).ReturnsAsync(new ServiceDirectory.Shared.Dto.ServiceDto
-            {
-                Id = 2,
-                Name = "Service",
-                Description = "Service Description",
-                ServiceType = ServiceDirectory.Shared.Enums.ServiceType.FamilyExperience
-            });
+            mockServiceDirectory.Setup(x => x.GetServiceById(It.IsAny<long>())).ReturnsAsync(
+                new ServiceDirectory.Shared.Dto.ServiceDto
+                {
+                    Id = 2,
+                    Name = "Service",
+                    Description = "Service Description",
+                    ServiceType = ServiceDirectory.Shared.Enums.ServiceType.FamilyExperience
+                });
 
             CreateReferralCommand createCommand = new(newReferral);
-            CreateReferralCommandHandler createHandler = new(TestDbContext, Mapper, mockServiceDirectory.Object, new Mock<ILogger<CreateReferralCommandHandler>>().Object);
+            CreateReferralCommandHandler createHandler = new(TestDbContext, Mapper, mockServiceDirectory.Object,
+                new Mock<ILogger<CreateReferralCommandHandler>>().Object);
 
             var activity = new Activity("TestActivity");
             activity.SetParentId(ActivityTraceId.CreateRandom(), ActivitySpanId.CreateRandom());
@@ -50,15 +55,15 @@ namespace FamilyHubs.Referral.Integration.Tests
             var result = await createHandler.Handle(createCommand, new CancellationToken());
 
             //Assert
-            result.Should().NotBe(0);
+            result.Should().NotBeNull();
             var actualService = TestDbContext.Referrals
                 .Include(x => x.Status)
-            .Include(x => x.UserAccount)
-            .Include(x => x.Recipient)
-            .Include(x => x.ReferralService)
-            .ThenInclude(x => x.Organisation)
-            .AsNoTracking()
-            .SingleOrDefault(s => s.Id == result.Id);
+                .Include(x => x.UserAccount)
+                .Include(x => x.Recipient)
+                .Include(x => x.ReferralService)
+                .ThenInclude(x => x.Organisation)
+                .AsNoTracking()
+                .SingleOrDefault(s => s.Id == result.Id);
             actualService.Should().NotBeNull();
 
             //todo: it's a bad idea to use the same code as the unit test uses to create the expected object
@@ -67,12 +72,64 @@ namespace FamilyHubs.Referral.Integration.Tests
 
             actualReferral.Should().BeEquivalentTo(newReferral.Referral, options =>
                 options.Excluding((IMemberInfo info) => info.Name.Contains("Id"))
-                       .Excluding((IMemberInfo info) => info.Name.Contains("Created"))
-                       .Excluding((IMemberInfo info) => info.Name.Contains("CreatedBy"))
-                       .Excluding((IMemberInfo info) => info.Name.Contains("LastModified"))
-                       .Excluding((IMemberInfo info) => info.Name.Contains("LastModifiedBy"))
-                       .Excluding((IMemberInfo info) => info.Name.Contains("Url"))
-                    );
+                    .Excluding((IMemberInfo info) => info.Name.Contains("Created"))
+                    .Excluding((IMemberInfo info) => info.Name.Contains("CreatedBy"))
+                    .Excluding((IMemberInfo info) => info.Name.Contains("LastModified"))
+                    .Excluding((IMemberInfo info) => info.Name.Contains("LastModifiedBy"))
+                    .Excluding((IMemberInfo info) => info.Name.Contains("Url"))
+            );
+        }
+
+        [Fact]
+        public async Task ThenCreateReferralCreatesConnectionRequestsSentMetric()
+        {
+            const long userOrganisationId = 123L;
+
+            var newReferral = new CreateReferralDto(TestDataProvider.GetReferralDto(),
+                new ConnectionRequestsSentMetricDto(userOrganisationId));
+            Mock<IServiceDirectoryService> mockServiceDirectory = new Mock<IServiceDirectoryService>();
+            mockServiceDirectory.Setup(x => x.GetOrganisationById(It.IsAny<long>())).ReturnsAsync(
+                new ServiceDirectory.Shared.Dto.OrganisationDto
+                {
+                    Id = 2,
+                    Name = "Organisation",
+                    Description = "Organisation Description",
+                    OrganisationType = ServiceDirectory.Shared.Enums.OrganisationType.VCFS,
+                    AdminAreaCode = "AdminAreaCode"
+                });
+
+            mockServiceDirectory.Setup(x => x.GetServiceById(It.IsAny<long>())).ReturnsAsync(
+                new ServiceDirectory.Shared.Dto.ServiceDto
+                {
+                    Id = 2,
+                    Name = "Service",
+                    Description = "Service Description",
+                    ServiceType = ServiceDirectory.Shared.Enums.ServiceType.FamilyExperience
+                });
+
+            CreateReferralCommand createCommand = new(newReferral);
+            CreateReferralCommandHandler createHandler = new(TestDbContext, Mapper, mockServiceDirectory.Object,
+                new Mock<ILogger<CreateReferralCommandHandler>>().Object);
+
+            var activity = new Activity("TestActivity");
+            activity.SetParentId(ActivityTraceId.CreateRandom(), ActivitySpanId.CreateRandom());
+            activity.Start();
+            Activity.Current = activity;
+            string expectedRequestCorrelationId = Activity.Current!.TraceId.ToString();
+
+            //Act
+            var result = await createHandler.Handle(createCommand, new CancellationToken());
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Id.Should().BeGreaterThan(0);
+
+            //var metric =
+            //    TestDbContext.ConnectionRequestsSentMetric.SingleOrDefault(m => m.ConnectionRequestId == result.Id);
+            var metric = TestDbContext.ConnectionRequestsSentMetric.SingleOrDefault();
+
+            metric.Should().NotBeNull();
+            metric!.RequestCorrelationId.Should().Be(expectedRequestCorrelationId);
         }
     }
 }
