9eb02b99c18c830e3710e9e1103d2d199f27ed92
Phil Davies <github@phildavies.co.uk>
2024-07-10 09:39:43 +0100

    actually run validators

--- src/FamilyHubs.Referral.Api/StartupExtensions.cs
+++ src/FamilyHubs.Referral.Api/StartupExtensions.cs
@@ -19,6 +19,7 @@ using FamilyHubs.SharedKernel.Identity;
 using FamilyHubs.SharedKernel.Security;
 using FamilyHubs.Referral.Core.ClientServices;
 using FamilyHubs.SharedKernel.Razor.Health;
+using FluentValidation;
 
 namespace FamilyHubs.Referral.Api;
 
@@ -167,6 +168,8 @@ public static class StartupExtensions
         });
 
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        services.AddValidatorsFromAssemblyContaining<CreateReferralCommand>();
 
         services.AddTransient<CorrelationMiddleware>();
         services.AddTransient<ExceptionHandlingMiddleware>();
