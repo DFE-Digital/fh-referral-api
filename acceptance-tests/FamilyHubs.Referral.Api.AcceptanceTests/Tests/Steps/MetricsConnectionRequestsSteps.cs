81c9e197c7a4b50c8b03c433a9fb4475017dadaa
Tina Gohil <tinagohil@Tinas-MacBook-Pro.local>
2024-08-06 16:59:32 +0100

    refactor: acceptance tests for api metrics

--- acceptance-tests/FamilyHubs.Referral.Api.AcceptanceTests/Tests/Steps/MetricsConnectionRequestsSteps.cs
+++ acceptance-tests/FamilyHubs.Referral.Api.AcceptanceTests/Tests/Steps/MetricsConnectionRequestsSteps.cs
@@ -1,8 +1,9 @@
-using FluentAssertions;
+using FamilyHubs.ReferralService.Shared.Dto;
 using System.Net;
 using FamilyHubs.Referral.Api.AcceptanceTests.Builders.Http;
 using FamilyHubs.Referral.Api.AcceptanceTests.Configuration;
-using FamilyHubs.Referral.Api.AcceptanceTests.Models;
+using FamilyHubs.ReferralService.Shared.Dto.CreateUpdate;
+using FamilyHubs.ReferralService.Shared.Dto.Metrics;
 
 namespace FamilyHubs.Referral.Api.AcceptanceTests.Tests.Steps;
 
@@ -12,7 +13,7 @@ namespace FamilyHubs.Referral.Api.AcceptanceTests.Tests.Steps;
 public class MetricsConnectionRequestsSteps
 {
     private readonly string _baseUrl;
-    private ConnectionRequest _request;
+    private CreateReferralDto _request;
     private HttpStatusCode _statusCode;
     public HttpResponseMessage lastResponse { get; private set; }
     public MetricsConnectionRequestsSteps()
@@ -31,15 +32,9 @@ public class MetricsConnectionRequestsSteps
 
     #region Given
 
-    public void GivenIHaveAConnectionMetricsRequest(long connectionRequestId, HttpStatusCode statusCode)
+    public void GivenIHaveAReferralsRequest()
     {
-        DateTimeOffset time = DateTimeOffset.UtcNow;
-        _request = new ConnectionRequest()
-        {
-            connectionRequestId = connectionRequestId,
-            httpResponseCode = statusCode,
-            requestTimestamp = time,
-        };
+        _request = GetCreateReferralDto();
     }
 
     #endregion Given
@@ -51,7 +46,7 @@ public class MetricsConnectionRequestsSteps
         Dictionary<string, string> headers = new Dictionary<string, string>() { };
         headers.Add("traceparent", new Guid().ToString());
         
-        lastResponse = await HttpRequestFactory.Put(_baseUrl, "api/metrics/connection-request", _request, bearerToken,
+        lastResponse = await HttpRequestFactory.Post(_baseUrl, "api/referrals", _request, bearerToken,
             headers, null);
         _statusCode = lastResponse.StatusCode;
 
@@ -61,4 +56,75 @@ public class MetricsConnectionRequestsSteps
     #endregion When
 
     #endregion Step Definitions
+
+    private static CreateReferralDto GetCreateReferralDto()
+    {
+        return new CreateReferralDto(GetReferralDto(), new ConnectionRequestsSentMetricDto(DateTimeOffset.UtcNow));
+    }
+
+    private static ReferralDto GetReferralDto()
+    {
+        return new ReferralDto
+        {
+            ReasonForSupport = "Reason For Support",
+            EngageWithFamily = "Engage With Family",
+            RecipientDto = new RecipientDto
+            {
+                Id = 2,
+                Name = "Joe Blogs",
+                Email = "JoeBlog@email.com",
+                Telephone = "078123456",
+                TextPhone = "078123456",
+                AddressLine1 = "Address Line 1",
+                AddressLine2 = "Address Line 2",
+                TownOrCity = "Town or City",
+                County = "County",
+                PostCode = "B30 2TV"
+            },
+            ReferralUserAccountDto = new UserAccountDto
+            {
+                Id = 2,
+                EmailAddress = "Bob.Referrer@email.com",
+                Name = "Bob Referrer",
+                PhoneNumber = "1234567890",
+                Team = "Team",
+                UserAccountRoles = new List<UserAccountRoleDto>()
+                    {
+                        new UserAccountRoleDto
+                        {
+                            UserAccount = new UserAccountDto
+                            {
+                                EmailAddress = "Bob.Referrer@email.com",
+                            },
+                            Role = new RoleDto
+                            {
+                                Name = "LaProfessional"
+                            }
+                        }
+                    },
+                ServiceUserAccounts = new List<UserAccountServiceDto>(),
+                OrganisationUserAccounts = new List<UserAccountOrganisationDto>(),
+            },
+            Status = new ReferralStatusDto
+            {
+                Id = 1,
+                Name = "New",
+                SortOrder = 0
+            },
+            ReferralServiceDto = new ReferralServiceDto
+            {
+                Id = 809,
+                Name = "Elop Mentoring",
+                Description = "Elop Mentoring",
+                Url = "www.service.com",
+                OrganisationDto = new OrganisationDto
+                {
+                    Id = 237,
+                    ReferralServiceId = 809,
+                    Name = "Elop Mentoring",
+                    Description = "Elop Mentoring",
+                }
+            }
+        };
+    }
 }
--- acceptance-tests/FamilyHubs.Referral.Api.AcceptanceTests/Tests/Stories/MetricsConnectionRequestsTests.cs
+++ acceptance-tests/FamilyHubs.Referral.Api.AcceptanceTests/Tests/Stories/MetricsConnectionRequestsTests.cs
@@ -23,37 +23,28 @@ public class MetricsConnectionRequestsTests
     }
 
     [Theory]
-    [InlineData("LaProfessional", 2, HttpStatusCode.OK, HttpStatusCode.NoContent)] // Happy path as LA professional
-    [InlineData("LaDualRole", 10, HttpStatusCode.InternalServerError,
-        HttpStatusCode.NoContent)] // Happy path as La Dual user
-    [InlineData("LaProfessional", 2, 2, HttpStatusCode.BadRequest)] // Invalid statusCode
-    [InlineData("LaProfessional", "0", HttpStatusCode.BadRequest,
-        HttpStatusCode.BadRequest)] // Invalid connectionRequestId
-    [InlineData("LaManager", 2, HttpStatusCode.OK, HttpStatusCode.Forbidden)] // Unauthorised as LA manager
-    [InlineData("VcsProfessional", 2, HttpStatusCode.OK, HttpStatusCode.Forbidden)] // Unauthorised as VCS professional
-    [InlineData("VcsDualRole", 2, HttpStatusCode.OK, HttpStatusCode.Forbidden)] // Unauthorised as VCS Dual User
-    [InlineData("VcsManager", 2, HttpStatusCode.OK, HttpStatusCode.Forbidden)] // Unauthorised as VCS Manager
-    [InlineData("DfeAdmin", 2, HttpStatusCode.OK, HttpStatusCode.Forbidden)] // Unauthorised as DfeAdmin
-    public void Service_Connection_Metrics_Endpoint_Returns_Expected_Status_Code(string role, long connectionRequestId,
-        HttpStatusCode statusCode, HttpStatusCode expectedStatusCode)
+    [InlineData("LaProfessional", HttpStatusCode.OK)] // Happy path as LA professional
+    [InlineData("LaDualRole", HttpStatusCode.OK)] // Happy path as La Dual user
+    [InlineData("LaManager", HttpStatusCode.Forbidden)] // Unauthorised as LA manager
+    [InlineData("VcsProfessional", HttpStatusCode.Forbidden)] // Unauthorised as VCS professional
+    [InlineData("VcsDualRole", HttpStatusCode.Forbidden)] // Unauthorised as VCS Dual User
+    [InlineData("VcsManager", HttpStatusCode.Forbidden)] // Unauthorised as VCS Manager
+    [InlineData("DfeAdmin", HttpStatusCode.Forbidden)] // Unauthorised as DfeAdmin
+    public void Connection_Request_Returns_Expected_Status_Code(string role, HttpStatusCode expectedStatusCode)
     {
         this.Given(s => _sharedSteps.GenerateBearerToken(role))
-            .And(s =>
-                _steps.GivenIHaveAConnectionMetricsRequest(connectionRequestId, statusCode))
+            .And(s => _steps.GivenIHaveAReferralsRequest())
             .When(s => _steps.WhenISendARequest(_sharedSteps.bearerToken))
             .Then(s => _sharedSteps.VerifyStatusCode(_steps.lastResponse, expectedStatusCode))
             .BDDfy();
     }
 
-    //Add all tests that make up the story to this class.
     [Theory]
-    [InlineData(2, HttpStatusCode.OK, HttpStatusCode.Unauthorized)] // Happy path as LA professional
-    public void Service_Connection_Metrics_Endpoint_Errors_Invalid_Bearer_Token(long connectionRequestId,
-        HttpStatusCode statusCode, HttpStatusCode expectedStatusCode)
+    [InlineData(HttpStatusCode.Unauthorized)]
+    public void Connection_Request_Endpoint_Errors_Invalid_Bearer_Token(HttpStatusCode expectedStatusCode)
     {
         this.Given(s => _sharedSteps.HaveAnInvalidBearerToken())
-            .And(s =>
-                _steps.GivenIHaveAConnectionMetricsRequest(connectionRequestId, statusCode))
+            .And(s => _steps.GivenIHaveAReferralsRequest())
             .When(s => _steps.WhenISendARequest(_sharedSteps.bearerToken))
             .Then(s => _sharedSteps.VerifyStatusCode(_steps.lastResponse, expectedStatusCode))
             .BDDfy();
